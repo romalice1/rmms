4dc56b41855bf0937e42a79d2ca0a067df1f1f5f
romalice1 <rommy_91@hotmail.fr>
2017-07-02 02:54:37 +0200

    Citizen list and new citizen UI

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'RIMS' });
 });
 
-/* GET test page. */
-router.get('/stuff', function(req, res, next) {
-  res.render('stuff', { title: 'Express' });
+/* Get - Register a new citizen. */
+router.get('/new-citizen', function(req, res, next) {
+  res.render('new-citizen', { title: 'RIMS' });
+});
+
+/* Get all registered citizens */
+router.get('/citizens', function(req, res, next) {
+  res.render('citizens', { title: 'RIMS' });
+});
+
+/* GET -  Go to dashboard */
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', { title: 'RIMS' });
+});
+
+/* GET -  Go to admin page */
+router.get('/admin', function(req, res, next) {
+  res.render('admin', { title: 'RIMS' });
 });
 
 module.exports = router;
