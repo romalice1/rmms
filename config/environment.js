9648aab53f490d11751b93876d0cad9e3cdae5bb
romalice1 <rommy_91@hotmail.fr>
2017-09-29 18:42:11 +0200

    Config environmen for heroku

--- config/environment.js
+++ config/environment.js
@@ -4,10 +4,10 @@ var ENV = {
 	host: 'http://localhost:3000',
 	DB: {
 		connAttr: {
-		  host     : 'localhost',
-		  user     : 'rims',
-		  password : 'rims',
-		  database : 'rwanda_migration_management_sys'
+		  host     : 'us-cdbr-iron-east-05.cleardb.net',
+		  user     : 'b0760f4472ffd2',
+		  password : 'bd238cad',
+		  database : 'heroku_1e419c5fa414b8a'
 		}
 	}
 }
